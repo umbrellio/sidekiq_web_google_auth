0fa8b45f3518ab58df354db7e5551b9fcef75c80
KirIgor <KirIgor@users.noreply.github.com>
2025-06-05 18:00:59 +0600

    fix

--- lib/sidekiq_web_google_auth/builder.rb
+++ lib/sidekiq_web_google_auth/builder.rb
@@ -8,13 +8,13 @@ module SidekiqWebGoogleAuth
 
     ARGUMENT_ERROR = "You must provide authorized_emails or authorized_emails_domains (or both)"
 
-    def provider(*args, authorized_emails: [], authorized_emails_domains: [], **options, &block)
+    def provider(config, *args, authorized_emails: [], authorized_emails_domains: [], **options, &block)
       invalid_arguments! if authorized_emails.empty? && authorized_emails_domains.empty?
       super("google_oauth2", *args, options.merge(name: "oauth"), &block)
 
       SidekiqWebGoogleAuth::Extension.authorized_emails = authorized_emails
       SidekiqWebGoogleAuth::Extension.authorized_emails_domains = authorized_emails_domains
-      Sidekiq::Web.register(SidekiqWebGoogleAuth::Extension)
+      config.register(SidekiqWebGoogleAuth::Extension)
     end
 
     private
