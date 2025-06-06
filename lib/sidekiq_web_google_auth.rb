1dbdd9439c49a5e564dee323b8794175750f818d
KirIgor <KirIgor@users.noreply.github.com>
2025-06-06 14:11:30 +0600

    fix

--- lib/sidekiq_web_google_auth.rb
+++ lib/sidekiq_web_google_auth.rb
@@ -8,4 +8,19 @@ OmniAuth.config.silence_get_warning = true
 
 module SidekiqWebGoogleAuth
   require_relative "sidekiq_web_google_auth/builder"
+  require_relative "sidekiq_web_google_auth/extension"
+
+  def self.load(config, client_id:, client_secret:, authorized_emails:, authorized_emails_domains:)
+    config.use(OmniAuth::Builder) do
+      provider(:google_oauth2, client_id, client_secret, name: "oauth")
+    end
+
+    SidekiqWebGoogleAuth::Extension.authorized_emails = authorized_emails
+    SidekiqWebGoogleAuth::Extension.authorized_emails_domains = authorized_emails_domains
+    config.register(
+      SidekiqWebGoogleAuth::Extension, name: "google-auth", tab: ["Logout"], index: ["logout"]
+    )
+
+    config.use(SidekiqWebGoogleAuth::Builder)
+  end
 end
